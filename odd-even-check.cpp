1b1d7572cdea539e8be0dd17baeaf7249b8b28b7
nnabicse <nnabi.cse@gmail.com>
2022-09-18 00:57:03 +0600

    odd even check

---
+++ odd-even-check.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    cin>>a;
+
+    if(a>1){
+        if(a%2 == 0){
+            cout<<a<<" is even"<<endl;
+        }
+        else{
+            cout<<a<<" is odd"<<endl;
+        }
+    }
+    else{
+        cout<<a<<" is odd";
+    }
+
+    
+
+
+
+
+
+    return 0;
+}
