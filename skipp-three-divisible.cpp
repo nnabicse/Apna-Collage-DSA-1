062304cd4915b270992f676c531f7b5956397269
nnabicse <nnabi.cse@gmail.com>
2022-09-23 01:21:20 +0600

    skip three divisible

---
+++ skipp-three-divisible.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=1; i<=100; i++){
+        if(i%3==0){
+            continue;
+        }
+        else{
+            cout<<i<<endl;
+        }
+    }
+
+
+
+
+    return 0;
+}
